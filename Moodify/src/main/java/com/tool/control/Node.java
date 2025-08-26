20aa8d69cbc969cad5d37acdf33ae161e2b7bba9
SamiduSamarasinghe <samarasinghesamidu@gmail.com>
2025-08-26 18:22:34 +0530

    Update Node.java

--- Moodify/src/main/java/com/tool/control/Node.java
+++ Moodify/src/main/java/com/tool/control/Node.java
@@ -7,14 +7,33 @@ public class Node {
     String songName;
     String artistName;
     String songPath;
+    int moodScore; //new field added(Sa)
+
 
 //    int skipCount;
 //    int playCount;
 //    int songScore;
 
-    public Node(String songName,String artistName,String songPath){
+    //old constructor
+    public  Node(String songName, String artistName, String songPath) {
         this.songName = songName;
         this.artistName = artistName;
         this.songPath = songPath;
+        this.moodScore = 5; // assign default score
+    }
+
+    //new constructor for when mood score is provided(Sa)
+    public Node(String songName,String artistName,String songPath ,int moodScore) {
+        this.songName = songName;
+        this.artistName = artistName;
+        this.songPath = songPath;
+        this.moodScore = moodScore;
+    }
+
+    public int getMoodScore() {
+        return moodScore;
+    }
+    public void setMoodScore(int moodScore) {
+        this.moodScore = moodScore;
     }
 }
