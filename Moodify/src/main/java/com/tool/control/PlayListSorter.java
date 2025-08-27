d23338063c39ed3c3e5e07a3bf56d57b99932e19
LakshanWC <lakshanwc@gmail.com>
2025-08-27 10:31:08 +0530

    Update PlayListSorter.java

--- Moodify/src/main/java/com/tool/control/PlayListSorter.java
+++ Moodify/src/main/java/com/tool/control/PlayListSorter.java
@@ -1,8 +1,13 @@
 package com.tool.control;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayListSorter {
 
-    private List<Node> playList;
+    private List<Node> playList = new ArrayList<>();
+
+    public void sortByMood(String mood){
+        
+    }
 }
