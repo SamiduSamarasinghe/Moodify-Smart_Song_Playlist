e1f214b137e2d200a4396e804e11905f82c20eba
LakshanWC <lakshanwc@gmail.com>
2025-08-28 10:49:19 +0530

    implementing sorting

--- Moodify/src/main/java/com/tool/control/PlayListSorter.java
+++ Moodify/src/main/java/com/tool/control/PlayListSorter.java
@@ -1,14 +1,70 @@
 package com.tool.control;
 
+import com.tool.model.DoublyLinkedList;
 import com.tool.model.Node;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PlayListSorter {
 
-    private List<Node> playList = new ArrayList<>();
+    public void sortByMood(DoublyLinkedList currentPlaylist,String mood){
+        
+        if(currentPlaylist != null){
+            switch(mood){
+                case "Calm":
+                    sort(currentPlaylist, 1);
+                    break;
 
-    public void sortByMood(String mood){
+                case "Neutral":
+                    sort(currentPlaylist, 2);
+                    break;
+
+                case "Energetic":
+                    sort(currentPlaylist, 3);                    
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+    
+    private void sort(DoublyLinkedList playList,int moodValue){
+        
+        Node calmHead = null , calmTail = null;
+        Node neutralHead = null , neutralTail = null;
+        Node energeticHead = null, energeticTail = null; 
+        
+        Node currentNode = playList.head;
+        
+        while(currentNode != null){
+            
+            Node nextNode = currentNode.nextNode;
+            //breake the link
+            currentNode.nextNode = null;
+            currentNode.previousNode = null;
+            
+            // go through each mood
+            switch(currentNode.getMoodScore()){
+                
+                case 1: //calm
+                    if(calmHead == null){
+                        calmHead = currentNode;
+                        calmTail = currentNode;
+                    }
+                    else{ // add to the end Node & swap the links
+                        calmTail.nextNode = currentNode;        
+                        currentNode.previousNode = calmTail;
+                        calmTail = currentNode;
+                    }
+                    break;
+            }
+            
+            
+            
+            currentNode = currentNode.nextNode;
+        }
+        
         
     }
 }
